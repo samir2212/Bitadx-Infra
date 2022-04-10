c5569122a9053afa921e4e96ac12739925e8d2c8
BitaDX GitLab CI <bitadx-ci@gitlab-ci.com>
2022-04-10 11:43:12 +0000

    Latest version 'main_394f67b9' deployment

--- variables.tf
+++ variables.tf
@@ -62,9 +62,9 @@ variable "ecs_php_app" {
 }
 
 variable "proxy_image" {
-  default = "840128973210.dkr.ecr.eu-central-1.amazonaws.com/proxy-dev:main_974f93f5_1"
+  default = "840128973210.dkr.ecr.eu-central-1.amazonaws.com/proxy-dev:main_394f67b9"
 }
 
 variable "php_app_image" {
-  default = "840128973210.dkr.ecr.eu-central-1.amazonaws.com/php_app-dev:main_974f93f5"
+  default = "840128973210.dkr.ecr.eu-central-1.amazonaws.com/php_app-dev:main_394f67b9"
 }
